db320c768d497ce23388d4a0df43ab03636f2abf
Estellio <pirko.vichmann@voco.ee>
2022-09-26 13:01:47 +0300

    functions

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
-const today = new Date()
+function greeting(name) {
+    return `Hello, ${name}`
+}
+let greet = greeting('Pirko')
+console.log(greet)
 
-let val
-val = today
-val = today.getMonth()
-val = today.getDay()
-val = today.getFullYear()
-val = today.getMinutes()
+const square = function(number) {
+    return number * number
+}
+let result = square(3)
+console.log(result)
 
-console.log(val)
+(function () {
+    console.log('function is running')
+})();
+
+(function (string) {
+    console.log(`Hello ${string}`)
+})(`Pirko`);
+
+const todo = {
+    function () {
+        console.log('add todo')
+    }
+}
